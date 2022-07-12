1d6504d24cd926a7e21080f5a9ebd75468a5c2d1
xandsc <samuelscav@hotmail.com>
2022-07-12 01:36:35 -0300

    update App Js

--- wall-of-shame/src/App.js
+++ wall-of-shame/src/App.js
@@ -2,59 +2,68 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Children } from 'react'
 
-const BaseElement = (props) => { 
+const BaseElement = (props) => {
   return <div className="App">
-  <header className="App-header">
-    HOME PAGE READY
-    {props.children}
-  </header>
-</div>
+    <header className="App-header">
+      HOME PAGE READY
+      {props.children}
+    </header>
+  </div>
 }
 
 // REST API
-const getRequestSample =  { 
+const getRequestSample = {
   method: 'GET',
   headers: new Headers(),
   mode: 'cors',
-  cache: 'default' 
+  cache: 'default'
 };
 
 function App() {
 
-  const [ pokemon, setPokemon ] = React.useState(``)
+  const [pokemon, setPokemon] = React.useState([])
 
   const onMount = () => {
-    console.log({ mingala: "noob"});
 
-    // Todo request, deve aguardar um response
-    // Timeout === the server did not response 
-    fetch(`https://pokeapi.co/api/v2/pokemon/ditto`, getRequestSample) 
-    .then(res => {
-      console.log({ "response": res, });
-      if ( res.status === 200 ) {
-        return res.json(); // Reading the body stream response and returning it to the next level
-      }
-      throw "nao tem pokemon"
+    console.log({ mingala: "noob" });
+
+    const pokemon1 = fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    const pokemon2 = fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+
+    var pokemonsArray = []
+
+    Promise.all([pokemon1, pokemon2]).then(values => {
+      return Promise.all(values.map(res => res.json()))
+
+    }).then(([pokemon1, pokemon2]) => {
+
+      var pokemonsArray = [pokemon1, pokemon2]
+      return pokemonsArray
+
+    }).then(answer => {
+      setPokemon(answer)
     }
     )
-    .then(answer => {
-      setPokemon(answer)
-    }).catch(e => console.log(`Could not fetch pokemon, error: ${e}`)).finally(() => { 
-      console.log("Fetch end")
-    });
+      .catch(e => {
+        console.log(`Could not fetch pokemon error: ${e}`)
+      }).finally(() => {
+        console.log("Fetch end")
+      })
   }
 
-  
   // onMount useEffect to fetch the first page render
   React.useEffect(onMount, [])
 
-  return (
-    <BaseElement>
-      <div>
-        {pokemon && pokemon.sprites && pokemon.sprites.back_default && <img src={pokemon.sprites.back_default} />}
-      </div>
-    </BaseElement>
-  );
+  return <BaseElement>
+    {
+      pokemon.length && pokemon.map(pkm => {
+        return <div>
+          {pkm.sprites && pkm.sprites.back_default && <img src={pkm.sprites.back_default} />}
+        </div>
+      }
+      )
+    }
+  </BaseElement>
 }
 
 export default App;
