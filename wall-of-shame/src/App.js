26ad57cf9ce828d115500227d9e2491b9034ef17
xandsc <samuelscav@hotmail.com>
2022-07-14 19:11:43 -0300

    battle poke front

--- wall-of-shame/src/App.js
+++ wall-of-shame/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Children } from 'react'
 
 const BaseElement = (props) => {
   return <div className="App">
     <header className="App-header">
-      HOME PAGE READY
+      Pokemon Battle
       {props.children}
     </header>
   </div>
@@ -24,11 +23,10 @@ function App() {
   const [pokemon, setPokemon] = React.useState([])
 
   const onMount = () => {
-
     console.log({ mingala: "noob" });
 
-    const pokemon1 = fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-    const pokemon2 = fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+    const pokemon1 = fetch('https://pokeapi.co/api/v2/pokemon/charizard')
+    const pokemon2 = fetch('https://pokeapi.co/api/v2/pokemon/charizard')
 
     var pokemonsArray = []
 
@@ -42,27 +40,38 @@ function App() {
 
     }).then(answer => {
       setPokemon(answer)
-    }
-    )
-      .catch(e => {
-        console.log(`Could not fetch pokemon error: ${e}`)
-      }).finally(() => {
-        console.log("Fetch end")
-      })
+
+    }).catch(e => {
+      console.log(`Could not fetch pokemon error: ${e}`)
+
+    }).finally(() => {
+      console.log("Fetch end")
+    })
   }
 
   // onMount useEffect to fetch the first page render
   React.useEffect(onMount, [])
 
   return <BaseElement>
-    {
-      pokemon.length && pokemon.map(pkm => {
-        return <div>
-          {pkm.sprites && pkm.sprites.back_default && <img src={pkm.sprites.back_default} />}
-        </div>
+    <div className="container">
+      {
+        pokemon.length && pokemon.map((pkm, index) => {
+
+          const isUp = index % 2
+          const imgKey = isUp ? 'back_default' : 'front_default'
+
+          return <div key={index} className={(isUp ? 'down' : 'up')}>
+
+            {pkm.sprites && pkm.sprites[imgKey] && <img src={pkm.sprites[imgKey]} />}
+            <span className={(isUp ? 'down-name' : 'up-name')}>{pkm.name}</span>
+          </div>
+        }
+        )
       }
-      )
-    }
+    </div>
+    <div>
+
+    </div>
   </BaseElement>
 }
 
